0970540734f1e1dcbfd18519bccdad759bad97f5
Marcus <marcusvini211@gmail.com>
2020-05-04 16:41:27 -0300

    Registers models to admin

--- tags/models.py
+++ tags/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=40)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.name} - active: {self.is_active}'
